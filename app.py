dcfcc2e932f812c56ca42232ca4fec4da87d27fc
tkderphu <quangphu.re050204@gmail.com>
2024-11-08 17:04:37 +0700

    ok

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from extensions.extensions import socketio
 from flask_cors import CORS
@@ -5,6 +7,10 @@ from database import db
 from blueprints.auth_blueprint import auth_bp
 from blueprints.user_blueprint import user_bp
 from blueprints.messenger_blueprint import messenger_bp
+from model import User
+from model import Conversation
+from model import Participation
+from model import Message
 from emitters import send_response_message  #
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/video_chat_flask'
@@ -23,31 +29,6 @@ app.register_blueprint(messenger_bp)
 # Ensure tables are created
 with app.app_context():
     db.create_all()
-
-# WebSocket event handlers
-@socketio.on('connect')
-def handle_connect():
-    print("Client connected")
-    socketio.emit('response', {'data': 'Connected to WebSocket server!'})
-
-@socketio.on('message')
-def handle_message(msg):
-    print(f"Received message: {msg}")
-    socketio.emit('test', {'data': f'Message received: {msg}'})
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print("Client disconnected")
-
-
-@app.route('/emit_message/<msg>')
-def emit_message(msg):
-    send_response_message(msg)  # Call the function to emit message
-    return {'status': 'Message emitted'}
-
-@socketio.on('/topic/private')
-def test(msg):
-    print(f"Custom message: {msg}")
 
 @socketio.on('/signal')
 def establish_video(signal):
